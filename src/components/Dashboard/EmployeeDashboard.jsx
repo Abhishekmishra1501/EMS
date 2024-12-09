7e3ad24fb06c0d8270fc69bdac70e025998afe6c
Abhishek Mishra <166721961+Abhishekmishra1501@users.noreply.github.com>
2024-12-10 04:32:40 +0530

    https://github.com/Abhishekmishra1501/EMS.git

---
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from 'react'
+import Header from '../other/Header'
+import TaskListNumbers from '../other/TaskListNumbers'
+import TaskList from '../TaskList/TaskList'
+
+const EmployeeDashboard = (props) => {
+
+  return (
+    <div className='p-10 bg-[#1C1C1C] h-screen'>
+        
+        <Header changeUser={props.changeUser} data={props.data}/>
+        <TaskListNumbers data={props.data} />
+        <TaskList data={props.data} />
+    </div>
+  )
+}
+
+export default EmployeeDashboard
